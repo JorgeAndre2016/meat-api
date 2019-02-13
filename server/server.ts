05272f9dafb0d5386cbb6202c4a4b164c29bbc77
Jorge <jorge_aquino@hotmail.com.br>
2019-02-13 00:48:20 -0200

    configurando acesso banco pelo mongoose

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,20 @@
 import * as restify from 'restify';
+import * as mongoose from 'mongoose';
+
 import { environment } from '../common/environment';
 import { Router } from '../common/router';
 
 export class Server {
 
     application: restify.Server;
+
+    initializeDb() {
+        (<any>mongoose).Promise = global.Promise
+        // url de conexão + opções de conexões
+        return mongoose.connect(environment.db.url, {
+            useNewUrlParser: true
+        })
+    }
 
     initRoutes(routers: Router[]): Promise<any> {
         return new Promise((resolve, reject) => {
@@ -18,7 +28,7 @@ export class Server {
 
                 // routes
 
-                for(let router of routers) {
+                for (let router of routers) {
                     router.applyRouter(this.application);
                 }
                 // this.application.get('/info', [
@@ -60,8 +70,10 @@ export class Server {
             }
         });
     };
-    
+
     bootstrap(routers: Router[] = []): Promise<Server> {
-        return this.initRoutes(routers).then(() => this);
+        return this.initializeDb().then(() =>
+            this.initRoutes(routers).then(() => this)
+        );
     };
 }
