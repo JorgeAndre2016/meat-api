6a5a459f65193dc1ab54c1c728a34078decac65e
Jorge <jorge_aquino@hotmail.com.br>
2019-02-13 23:23:06 -0200

    implementando emit de evento para filtrar dados no resturn do mongo

--- common/router.ts
+++ common/router.ts
@@ -1,6 +1,19 @@
 import * as restify from 'restify';
+import { EventEmitter } from 'events';
 
 // class abstrata usada para mapear as rotas
-export abstract class Router {
+export abstract class Router extends EventEmitter {
     abstract applyRouter(application: restify.Server);
+
+    render(response: restify.Response, next: restify.Next){
+        return (document) => {
+            if(document){
+                this.emit('beforeRender', document);
+                response.json(document);
+            }else {
+                response.send(404);
+            };
+            return next();
+        }
+    }
 }
