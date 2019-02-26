3c937d03598651bf5dd34b32e2bf1dc57c4767f6
Jorge <jorge_aquino@hotmail.com.br>
2019-02-25 23:42:47 -0300

    insert tratamento de erros

--- server/error.handler.ts
+++ server/error.handler.ts
@@ -14,6 +14,14 @@ export const handleError = (req, resp, err, done) => {
             break;
         case 'ValidationErro':
             err.statisCode = 400;
+            const messages: any[] = [];
+            err.errors.forEach((msg) => {
+                messages.push({message: msg.message});
+            });            
+                   
+            err.toJSON = () => ({
+                errors: messages
+            });
             break;
         default:
             break;
