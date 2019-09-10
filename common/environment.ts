b92b197b00bbf6a2cc9edde2c16b2a76f6ce6904
Jorge <jorge_aquino@hotmail.com.br>
2019-09-09 23:48:36 -0300

    insert variables

--- common/environment.ts
+++ common/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     // caso não sejá informado a porta como parâmetro a default será ativada '3000'
     server: { port: process.env.SERVER_PORT || 3000 },
-    db: {url: process.env.DB_URL || 'mongodb://localhost/meat-api'},
+    db: { url: process.env.DB_URL || 'mongodb://localhost/meat-api' },
     security: { saltRounds: process.env.SALT_ROUNDS || 10 }
 }
